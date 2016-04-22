c07120f1f4a6b7a1501f12373ee7467826c6c776
aparker611 <adam3692@hotmail.co.uk>
2016-04-22 16:35:45 +0100

    Updated gemfile and root as well as styled header-sidebar and main expense

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  scope module: 'public' do
+    root 'dashboard#welcome'    
+  end
 end
