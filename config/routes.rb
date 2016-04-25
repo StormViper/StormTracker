82b2c2d6b165420152af5a9f05442f4575b2b0fc
aparker611 <adam3692@hotmail.co.uk>
2016-04-25 12:08:17 +0100

    Added user table, updated sign_in and sign_up views with some style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,5 +57,9 @@ Rails.application.routes.draw do
 
   scope module: 'public' do
     root 'dashboard#welcome'    
+  end
+
+  scope module: 'member' do
+    get '/users/signup', to: 'users#signup'
   end
 end
