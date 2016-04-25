daef3d7804cdbf9febb3a5ac8bb9f9a26b573b31
aparker611 <adam3692@hotmail.co.uk>
2016-04-25 15:06:31 +0100

    @

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+              :controllers => { :registrations => "member/registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -61,5 +62,6 @@ Rails.application.routes.draw do
 
   scope module: 'member' do
     get '/users/signup', to: 'users#signup'
+    post '/users/signup', to: 'registrations#create'
   end
 end
