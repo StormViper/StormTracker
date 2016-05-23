14386226ef8fd94eff499ffaa8d58f94ff73c034
aparker611 <adam3692@hotmail.co.uk>
2016-05-23 14:16:05 +0100

    Added indentation removed console log

--- app/controllers/member/dashboard_controller.rb
+++ app/controllers/member/dashboard_controller.rb
@@ -1,18 +1,17 @@
 class Member::DashboardController < ApplicationController
-	def home
-		@expense = Expense.new
-	  @user_expense = current_user.expense
-	end
+  def home
+    @expense = Expense.new
+  @user_expense = current_user.expense
+  end
 
-	def upgrade
+  def upgrade
+  end
 
-	end
-
-	def get_user
-		if user_signed_in?
-			render json: {user: current_user}
-		else
-			render json: {guest: "not signed in"}
-		end
-	end
+  def get_user
+  if user_signed_in?
+    render json: {user: current_user}
+  else
+    render json: {guest: "not signed in"}
+  end
+  end
 end
