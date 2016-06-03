6c287d33d2bf547e930d0034f5a2749841b1b129
aparker611 <adam3692@hotmail.co.uk>
2016-06-03 10:36:39 +0100

    Sorted out some styling

--- app/controllers/member/settings_controller.rb
+++ app/controllers/member/settings_controller.rb
@@ -1,4 +1,8 @@
 class Member::SettingsController < ApplicationController
+	before_filter :authenticate_user!
   def home
+  	@user = current_user
+  	@other = 'other'
+  	@company = current_user.comp
   end
 end
