d928d6675802ca4ab768f1896194c969e71c77aa
aparker611 <adam3692@hotmail.co.uk>
2016-06-15 10:43:31 +0100

    Added expense creation for companies and branches

--- app/services/expenses/save_handler.rb
+++ app/services/expenses/save_handler.rb
@@ -1,40 +1,28 @@
 class SaveHandler
 	attr_accessor :user, :expense
-	def initialize(expense, user, params)
-		@expense, @user, @params = expense, user, params
+	def initialize(expense, user, params, company)
+		@expense, @user, @params, @company = expense, user, params, company
 	end
 
 	def save_hard
-		if @expense.new_record?
-			if @expense.save
-				if @params[:expense][:company] == "1"
-					@expense.toggle! :company?
-				end
-
-				if @expense.company? == true
-					if @params[:Branch]
-						@branch = Branch.find_by_id(@params[:Branch])
-						@branch.expense << @expense
-					else
-					  user.comp.first.expense << @expense
-					  @expense.status = "Waiting to be confirmed"
-					  @expense.save
-					end
-				else
-					@expense.status = "Personal"
-					@expense.save
-				end
-				@user.expense << @expense
-				return "success"
-			else
-				return "error"
-			end
-		else 
-			if @expense.save
-				return "success"
-			else
-				return "error"
-			end
-		end
-	end
+  if @expense.save
+    if @expense.company? == true
+      if @user.branch.count > 0 && @company.branch.count > 0
+        @branch = @user.branch.first
+        @branch.expense << @expense
+      else
+        @company.expense << @expense
+        @expense.status = "Waiting to be confirmed"
+        @expense.save
+      end
+    else
+      @expense.status = "Personal"
+      @expense.save
+    end
+      @user.expense << @expense
+      return "success"
+  else
+    return "error"
+  end
+end
 end
