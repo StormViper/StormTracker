65d64c292ae64f1313b61f16cac6d8e7c2d83d03
Adam Parker <adam@Opss-Mac-mini.local>
2016-09-07 11:15:32 +0100

    Added some payment access permissions for company and users if they have not paid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :calculate_salary
+  helper_method :calculate_salary, :check_access
+
+  def check_company_access(company)
+    if user_signed_in?
+    if company.subscription == "free"
+      redirect_to root_path
+      flash[:danger] = "You're company has not paid for this month's subscription please contact your administrator!"
+    end
+  end
+  end
+
+  def check_access
+    if user_signed_in?
+    if current_user.subscription == "free"
+      redirect_to root_path
+      flash[:danger] = "You have not paid for this month's subscription!"
+    else
+    end
+  end
+  end
 
 def calculate_salary
   return if !user_signed_in?
